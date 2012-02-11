dfa4b3451d4acb9d9aa65b4da6b3c77150901467
Jell <jean-louis@icehouse.se>
2012-02-12 00:41:12 +0100

    Exercises 1.6 to 1.8

--- src/sicp/chapter-1/section-1.clj
+++ src/sicp/chapter-1/section-1.clj
@@ -74,3 +74,61 @@
 ;; then you don't want to run (t 0 (p)) in normal order evaluation (infinite
 ;; recursion), but it's ok in applicative order evalutation because only x needs to
 ;; be resolved.
+
+;; 1.6
+
+(defn new-if [predicate then-clause else-clause]
+  (cond predicate then-clause
+        :else else-clause))
+
+;; Both then-clause and else-clause will be executed, which defeats
+;; the point of 'if'
+
+;; 1.7
+
+(defn abs [n]
+  {:post [(<= 0 %)]}
+  (if (< 0 n) n (- n)))
+
+(defn square [n]
+  (* n n))
+
+(defn average [& args]
+  {:pre [(< 0 (count args))]}
+  (/ (apply + args) (count args)))
+
+(defn good-enough? [method guess x]
+  {:pre [(not (zero? x))]}
+  (> 0.001 (abs (/ (- (method guess) x) x))))
+
+(defn improve [guess x]
+  {:pre [(< 0 guess)]}
+  (average guess (/ x guess)))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? square guess x)
+    guess
+    (recur (improve guess x) x)))
+
+(defn sqrt [x]
+  {:post [(<= 0 x)]}
+  (if (zero? x) 0.0
+      (sqrt-iter 1.0 x)))
+
+;; 1.8
+
+(defn cube [x]
+  (* x x x))
+
+(defn improve-cube [guess x]
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(defn cube-root-iter [guess x]
+  (if (good-enough? cube guess x)
+    guess
+    (recur (improve-cube guess x) x)))
+
+(defn cube-root [x]
+  (if (zero? x)
+    0.0
+    (cube-root-iter 1.0 x)))
