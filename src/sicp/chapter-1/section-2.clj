9e3cbd702e785a5904b460003b8081ed178782a9
Jell <jean-louis@icehouse.se>
2012-03-26 22:35:13 +0200

    Solved 1.19

--- src/sicp/chapter-1/section-2.clj
+++ src/sicp/chapter-1/section-2.clj
@@ -286,3 +286,30 @@
   "iterative fast multiplication"
   [a b]
   (fast-*-iter 0 a b))
+
+;; 1.19
+
+;; Tpq  = (a, b) -> bq + aq + ap , bp + aq
+;; Tpq2 = (a, b) -> (bp + aq)q + (bq + aq + ap)q + (bq + aq + ap)p, (bp + aq)p + (bq + aq + ap)q
+;;      = (a, b) -> bpq + aq^2 + bq^2 + aq^2 + apq + bpq + apq + ap^2, bp^2 + apq + bq^2 + aq^2 + apq
+;;      = (a, b) -> b(2pq + q^2) + a(2q^2 + 2pq + p^2 ), b(p^2 + q^2) + a(2pq + q^2)
+;;      = (a, b) -> b(2pq + q^2) + a(p^2 + q^2) + a(2pq + q^2), b(p^2 + q^2) + a(2pq + q^2)
+
+;; p' = (p^2 + q^2)
+;; q' = (2pq + q^2)
+
+(defn fib-iter [a b p q count]
+  (cond (= count 0) b
+        (even? count) (recur a
+                             b
+                             (+ (square p ) (square q))
+                             (+ (* 2 p q) (square q))
+                             (/ count 2))
+        :else (recur (+ (* b q) (* a q) (* a p))
+                     (+ (* b p) (* a q))
+                     p
+                     q
+                     (- count 1))))
+
+(defn fast-fib [n]
+  (fib-iter 1 0 0 1 n))
