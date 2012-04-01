e686444012141c2b2f08a3e6d17fb99641ca0b28
Jell <jean-louis@icehouse.se>
2012-04-01 14:23:18 +0200

    Piff.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject sicp "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "My collection of exercises from the wizzard book."
   :dependencies [[org.clojure/clojure "1.3.0"]])
